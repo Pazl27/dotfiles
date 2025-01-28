9fad9998d3453310091574174be13ea25810e8cc
Paul <jaul.ponas@gmail.com>
2025-01-28 22:16:30 +0100

    alpha stuff

--- .config/nvim/lua/paul/plugins/alpha.lua
+++ .config/nvim/lua/paul/plugins/alpha.lua
@@ -190,6 +190,7 @@ return {
         dashboard.button("f", "󰈞 " .. "Find file", "<cmd>Telescope find_files<CR>"),
         dashboard.button("g", "󰊄 " .. "Live grep", "<cmd>Telescope live_grep<CR>"),
         dashboard.button("r", " " .. "Recent files", "<cmd> Telescope oldfiles <cr>"),
+        dashboard.button("t", " " .. "Typr", "<cmd> Typr <cr>"),
         dashboard.button("o", "󰇈 " .. "Obsidian", "<cmd>cd ~/notes/personal<CR>"),
         dashboard.button("c", " " .. "Configuration", "<cmd>cd ~/.config/nvim/<CR><cmd>edit $MYVIMRC<CR>"),
         dashboard.button("u", " " .. "Update plugins", "<cmd>Lazy sync<CR>"),
@@ -198,17 +199,6 @@ return {
       },
       position = "center",
     }
-    local footer = {
-      type = "text",
-      val = function()
-        return "[" .. cdir .. "]"
-      end,
-      opts = {
-        position = "center",
-        hl = "AlphaFooter"
-      },
-    }
-
     local config = {
       layout = {
         header,
@@ -239,94 +229,29 @@ return {
   end
 }
 
--- return {
---   "goolord/alpha-nvim",
---   event = "VimEnter",
---   enabled = true,
---   init = false,
---   opts = function()
---     local dashboard = require("alpha.themes.dashboard")
---     local logo = [[
 --       ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
 --       ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
 --       ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
 --       ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
 --       ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
 --       ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
---     ]]
---
---     -- dashboard.section.header.val = {
---     --    [[=================     ===============     ===============   ========  ========]],
---     --    [[\\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //]],
---     --    [[||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||]],
---     --    [[|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||]],
---     --    [[||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||]],
---     --    [[|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||]],
---     --    [[||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||]],
---     --    [[|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||]],
---     --    [[||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||]],
---     --    [[||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||]],
---     --    [[||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||]],
---     --    [[||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||]],
---     --    [[||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||]],
---     --    [[||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||]],
---     --    [[||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||]],
---     --    [[||.=='    _-'                                                     `' |  /==.||]],
---     --    [[=='    _-'                        N E O V I M                         \/   `==]],
---     --    [[\   _-'                                                                `-_   /]],
---     --    [[ `''                                                                      ``' ]],
---     -- }
---     dashboard.section.header.val = vim.split(logo, "\n")
---
---     dashboard.section.buttons.val = {
---       dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
---       dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
---       dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
---       dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
---       dashboard.button("c", " " .. " Config", "<cmd>edit $MYVIMRC<CR>"),
---       dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
---       dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
---     }
---     for _, button in ipairs(dashboard.section.buttons.val) do
---       button.opts.hl = "AlphaButtons"
---       button.opts.hl_shortcut = "AlphaShortcut"
---     end
---     dashboard.section.header.opts.hl = "AlphaHeader"
---     dashboard.section.buttons.opts.hl = "AlphaButtons"
---     dashboard.section.footer.opts.hl = "AlphaFooter"
---     dashboard.opts.layout[1].val = 8
---     return dashboard
---   end,
---   config = function(_, dashboard)
---     -- close Lazy and re-open when the dashboard is ready
---     if vim.o.filetype == "lazy" then
---       vim.cmd.close()
---       vim.api.nvim_create_autocmd("User", {
---         once = true,
---         pattern = "AlphaReady",
---         callback = function()
---           require("lazy").show()
---         end,
---       })
---     end
---
---     require("alpha").setup(dashboard.opts)
---
---     vim.api.nvim_create_autocmd("User", {
---       once = true,
---       pattern = "LazyVimStarted",
---       callback = function()
---         local stats = require("lazy").stats()
---         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
---         dashboard.section.footer.val = "⚡ Neovim loaded "
---             .. stats.loaded
---             .. "/"
---             .. stats.count
---             .. " plugins in "
---             .. ms
---             .. "ms"
---         pcall(vim.cmd.AlphaRedraw)
---       end,
---     })
---   end,
--- }
+
+--    "=================     ===============     ===============   ========  ========"
+--    "\\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //"
+--    "||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||"
+--    "|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||"
+--    "||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||"
+--    "|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||"
+--    "||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||"
+--    "|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||"
+--    "||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||"
+--    "||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||"
+--    "||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||"
+--    "||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||"
+--    "||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||"
+--    "||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||"
+--    "||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||"
+--    "||.=='    _-'                                                     `' |  /==.||"
+--    "=='    _-'                        N E O V I M                         \/   `=="
+--    "\   _-'                                                                `-_   /"
+--    " `''                                                                      ``' "
