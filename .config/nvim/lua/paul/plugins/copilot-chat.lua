ddd8745c32b8717fbbd7a4703054cf0b1e1b6cd0
Paul <jaul.ponas@gmail.com>
2025-01-30 17:51:37 +0100

    chatbre stuff

--- .config/nvim/lua/paul/plugins/copilot-chat.lua
+++ .config/nvim/lua/paul/plugins/copilot-chat.lua
@@ -1,20 +1,21 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
+    build = "make tiktoken",                          -- Only on MacOS or Linux
     opts = {
+      -- See Configuration section for options
       debug = true, -- Enable debugging
       show_help = false,
-    window = {
-      layout = 'float',
-      title = 'Chat Bre',
-      border = 'rounded',
-    }
+      window = {
+        layout = 'float',
+        title = 'Chat Bre',
+        border = 'rounded',
+      }
     },
-
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
