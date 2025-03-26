18130950901740c5b5f48c03270692766e9d4c92
Paul <jaul.ponas@gmail.com>
2025-03-26 09:41:13 +0100

    script for switching wallpapers

---
+++ .config/scripts/rofi/wallpaper_switcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+picture_list=$(find ~/Pictures/wallpaper/gruvbox -type f)
+
+chosen_picture=$(echo -e "$toggle\n$picture_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wallpaper: " )
+
+if [ -z "$chosen_picture" ]; then
+  exit
+else
+  notify-send "Setting wallpaper to $chosen_picture"
+
+  swww img -t none --transition-duration 0 $chosen_picture
+fi
