8a30fa6ee0af98651e233c7cb71db156a56a01ef
Paul <jaul.ponas@gmail.com>
2025-04-10 16:32:36 +0200

    yazi script for popup

---
+++ .config/scripts/yazi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+hyprctl dispatch togglespecialworkspace drop
+
+# Nur starten, wenn es nicht läuft
+if ! pgrep -f "kitty.*yazi" > /dev/null; then
+  kitty --class dropterm -e yazi &
+  sleep 0.5
+  hyprctl dispatch focuswindow "class:dropterm"
+fi
