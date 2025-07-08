2f74567b1afff73dd0749f852a0e777838acc58a
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-07-08 02:02:49 +0200

    stuff

--- .config/nvim/lua/paul/plugins/treesitter.lua
+++ .config/nvim/lua/paul/plugins/treesitter.lua
@@ -1,41 +1,40 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "OXY2DEV/markview.nvim",
-    },
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  dependencies = {
+    "OXY2DEV/markview.nvim",
+  },
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
 
-        ensure_installed = {
-          "bash",
-          "css",
-          "dockerfile",
-          "go",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "python",
-          "rust",
-          "yaml",
-          "cmake",
-          "cpp",
-          "c",
-          "java",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "toml",
-          "yaml"
-        }
-      })
-    end
-  }
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "rust",
+        "yaml",
+        "cmake",
+        "cpp",
+        "c",
+        "java",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "toml",
+        "yaml"
+      }
+    })
+  end
 }
